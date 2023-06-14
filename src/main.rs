016ca56dbe78fb750cd9dff2622211b677fec1f7
sspaeti <simu@sspaeti.com>
2023-06-14 10:37:28 +0200

    using markdown parsing instead of HTML

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use mdbook::book::Book;
 use mdbook::errors::Error;
 use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
-use regex::Regex;
+// use regex::Regex;
 use serde_json;
 use std::io;
 use std::process;
 use clap;
+
+use pulldown_cmark::{Event, Options, Parser, Tag, CodeBlockKind};
+use pulldown_cmark_to_cmark::cmark;
 
 pub struct AdmonitionConverter;
 
@@ -20,23 +23,61 @@ impl Preprocessor for AdmonitionConverter {
         "admonition-converter"
     }
 
+    // returning Markdown: preprocessor.admonish seems not to work with it
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
-        eprintln!("Running {} preprocessor", self.name());
-        let re = Regex::new(r"> \[!(\w+)\]\s*\n> (.*)").unwrap();
-        book.for_each_mut(|item: &mut mdbook::BookItem| {
-            if let mdbook::BookItem::Chapter(chapter) = item {
-                let original_content = chapter.content.clone();
-                chapter.content = re.replace_all(&chapter.content, "```admonish $1\n$2\n```").to_string();
-                if original_content != chapter.content {
-                    println!("Admonition converted in chapter '{}'", chapter.name);
-                    println!("Original content: {}", original_content);
-                    println!("Converted content: {}", chapter.content);
-                }
-            }
-        });
-        Ok(book)
-    }
+            eprintln!("11: Running {} preprocessor", self.name());
+            let parser_options = Options::all();
+            book.for_each_mut(|item: &mut mdbook::BookItem| {
+                if let mdbook::BookItem::Chapter(chapter) = item {
+                    let events = Parser::new_ext(&chapter.content, parser_options).collect::<Vec<_>>();
+                    let mut new_events = Vec::new();
+                    let mut in_admonition = false;
+                    let mut admonition_type = String::new();
+                    for event in events {
+                        match event {
+                            Event::Start(Tag::BlockQuote) => {
+                                in_admonition = true;
+                            }
+                            Event::End(Tag::BlockQuote) => {
+                                in_admonition = false;
+                                new_events.push(Event::Text(format!("```admonish {}\n", admonition_type).into()));
+                                new_events.push(Event::End(Tag::CodeBlock(CodeBlockKind::Fenced(admonition_type.clone().into()))));
 
+                            }
+                            Event::Text(text) if in_admonition && text.starts_with("[!") => {
+                                admonition_type = text.trim_start_matches("[!").trim_end_matches("]").to_string();
+                            }
+                            Event::Text(text) if in_admonition => {
+                                new_events.push(Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(admonition_type.clone().into()))));
+                                new_events.push(Event::Text(text));
+                            }
+                            _ => new_events.push(event),
+                        }
+                    }
+                    let mut new_content = String::new();
+                    cmark(new_events.into_iter(), &mut new_content).unwrap();
+                    chapter.content = new_content;
+                }
+            });
+            Ok(book)
+    }
+    // // running with Regex
+    // fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
+    //     eprintln!("Running {} preprocessor", self.name());
+    //     let re = Regex::new(r"> \[!(\w+)\]\s*\n> (.*)").unwrap();
+    //     book.for_each_mut(|item: &mut mdbook::BookItem| {
+    //         if let mdbook::BookItem::Chapter(chapter) = item {
+    //             let original_content = chapter.content.clone();
+    //             chapter.content = re.replace_all(&chapter.content, "```admonish $1\n$2\n```").to_string();
+    //             if original_content != chapter.content {
+    //                 eprintln!("Admonition converted in chapter '{}'", chapter.name);
+    //                 eprintln!("Original content: {}", original_content);
+    //                 eprintln!("Converted content: {}", chapter.content);
+    //             }
+    //         }
+    //     });
+    //     Ok(book)
+    // }
 
     fn supports_renderer(&self, renderer: &str) -> bool {
         renderer != "not-supported"
