93718c43b2a8c6e53afbc044814e665971300443
sspaeti <simu@sspaeti.com>
2023-06-20 12:13:02 +0200

    simplyfy with synchrounous request

--- src/rest.rs
+++ src/rest.rs
@@ -1,36 +1,30 @@
 use std::error::Error;
-use tokio::runtime::Runtime;
-use url::Url;
 
 pub fn check_link(url: &str) -> Result<String, Box<dyn Error>> {
-    // Create a Tokio runtime
-    let rt = Runtime::new()?;
-    
-    // Spawn a new Tokio task to execute the async code
-    let result = rt.block_on(async move {
-        // Parse the URL to handle relative URLs
-        let parsed_url = Url::parse(url)?;
-        // Make a GET request using reqwest
-
-        // Make a GET request using reqwest
-        let response = reqwest::get(parsed_url.as_str()).await?;
-        
-        // Check if the response was successful
-        if response.status().is_success() {
-            Ok(format!("The URL is valid: {}", url))
-        } else {
-            Ok(format!("The URL returned an error: {}", response.status()))
-        }
-    });
-
-    match result {
-        Ok(message) => {
-            println!("{}", message);
-            Ok(message)
-        }
+    match reqwest::blocking::get(url) {
+        Ok(response) => {
+            if response.status().is_success() {
+                Ok(format!("The URL is valid: {}", url))
+            } else {
+                Ok(format!("The URL returned an error: {}, {}", response.status(), response.url()))
+            }
+        },
         Err(err) => {
-            eprintln!("Error: {}", err);
-            Err(err)
-        }
+            if err.is_redirect() {
+                Err(format!("Redirection error for URL {}: {}", url, err).into())
+            } else if err.is_timeout() {
+                Err(format!("Timeout error for URL {}: {}", url, err).into())
+            } else if err.is_body() {
+                Err(format!("Body error for URL {}: {}", url, err).into())
+            } else if err.is_status() {
+                Err(format!("Status code error for URL {}: {}", url, err).into())
+            } else if err.is_builder() {
+                Err(format!("Builder error for URL {}: {}", url, err).into())
+            } else if err.is_decode() {
+                Err(format!("Decode error for URL {}: {}", url, err).into())
+            } else {
+                Err(format!("Unknown error for URL {}: {}", url, err).into())
+            }
+        },
     }
 }
